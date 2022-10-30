610a76a52011d0aa2f2b9df26762b6094c387eca
AbdErrazak KENNICHE <91481473+abderrazak-hash@users.noreply.github.com>
2022-10-30 20:12:27 +0100

    Update invoice_webview.dart

--- lib/core/page/payment/invoice_webview.dart
+++ lib/core/page/payment/invoice_webview.dart
@@ -25,30 +25,32 @@ class _InvoiceWebViewState extends State<InvoiceWebView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: WebView(
-        initialUrl:
-            'https://test.6o9.live/api/Incoice/SellInvoice/${widget.invoiceId}',
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (c) {
-          ctrl = c;
-        },
-        onPageFinished: (c) async {
-          Uint8List? webConv;
-          String html = await ctrl!.runJavascriptReturningResult(
-              "encodeURIComponent(document.documentElement.outerHTML)");
-          html = Uri.decodeComponent(html);
-          html = html.substring(1, html.length - 1);
-          webConv = await WebcontentConverter.contentToImage(
-            content: html,
-          );
-          await SunmiPrinter.setAlignment(SunmiPrintAlign.CENTER);
-          await SunmiPrinter.startTransactionPrint(true);
-          await SunmiPrinter.printImage(webConv);
-          await SunmiPrinter.lineWrap(1);
-          await SunmiPrinter.exitTransactionPrint(true);
-          await SunmiPrinter.cut();
-        },
+    return Material(
+      child: Scaffold(
+        body: WebView(
+          initialUrl:
+              'https://test.6o9.live/api/Incoice/SellInvoice/${widget.invoiceId}',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (c) {
+            ctrl = c;
+          },
+          onPageFinished: (c) async {
+            Uint8List? webConv;
+            String html = await ctrl!.runJavascriptReturningResult(
+                "encodeURIComponent(document.documentElement.outerHTML)");
+            html = Uri.decodeComponent(html);
+            html = html.substring(1, html.length - 1);
+            webConv = await WebcontentConverter.contentToImage(
+              content: html,
+            );
+            await SunmiPrinter.setAlignment(SunmiPrintAlign.CENTER);
+            await SunmiPrinter.startTransactionPrint(true);
+            await SunmiPrinter.printImage(webConv);
+            await SunmiPrinter.lineWrap(1);
+            await SunmiPrinter.exitTransactionPrint(true);
+            await SunmiPrinter.cut();
+          },
+        ),
       ),
     );
   }
